ce513fc1a99f6981019f8470994fa7ff55bc2d21
David Glasser <glasser@davidglasser.net>
2025-06-02 08:13:59 -0700

    Remove workaround

--- index.ts
+++ index.ts
@@ -4,9 +4,6 @@ import express, { Request } from 'express'
 import { gql } from 'graphql-tag'
 import http from 'http'
 
-export type ApolloServerForExpressMiddleware<C extends BaseContext> =
-  Parameters<typeof expressMiddleware<C>>[0]
-
 export type Context = { req: Request }
 
 const typeDefs = gql`
@@ -22,13 +19,7 @@ await apollo.start()
 
 app.use(
   expressMiddleware<Context>(
-    // Issue: TS reports an error on `expressMiddleware<Context>(apollo, ...)`:
-    //   Argument of type
-    //   'import(".../@apollo/server/dist/esm/ApolloServer", { with: { "resolution-mode": "import" } }).ApolloServer<RC>'
-    //   is not assignable to parameter of type
-    //   'import(".../@apollo/server/dist/cjs/ApolloServer").ApolloServer<Context>'.
-    //   Types have separate declarations of a private property 'internals'.ts(2345)
-    apollo as unknown as ApolloServerForExpressMiddleware<Context>,
+    apollo,
     { context: async ({ req }) => ({ req }) }
   )
 )
